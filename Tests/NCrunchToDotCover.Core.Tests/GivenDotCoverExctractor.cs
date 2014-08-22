76a6035a80e3df14850b130e45b8d837b78dac63
Alberto Monteiro <alberto.monteiro@live.com>
2014-08-22 00:48:20 -0300

    First tests

---
+++ Tests/NCrunchToDotCover.Core.Tests/GivenDotCoverExctractor.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NCrunchToDotCover.Core.DotCover;
+
+namespace NCrunchToDotCover.Core.Tests
+{
+    [TestClass]
+    public class GivenDotCoverExctractor
+    {
+        [TestMethod]
+        public void CanDeserializedXmlInClass()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Root));
+            var root = xmlSerializer.Deserialize(new StreamReader(@"dotCoverCoverage.xml"));
+            Assert.IsNotNull(root);
+        }
+    }
+}
---
+++ Tests/NCrunchToDotCover.Core.Tests/GivenNCrunchExctractor.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NCrunchToDotCover.Core.NCrunch;
+
+namespace NCrunchToDotCover.Core.Tests
+{
+    [TestClass]
+    public class GivenNCrunchExctractor
+    {
+        [TestMethod]
+        public void CanDeserializedXmlInClass()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Solution));
+            var solution = xmlSerializer.Deserialize(new StreamReader(@"NCrunchCoverage.xml"));
+            Assert.IsNotNull(solution);
+        }
+    }
+}
